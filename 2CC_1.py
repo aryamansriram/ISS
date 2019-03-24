6dd3c279b0aad22a69fcf67f82dd5144c6a955cc
Aryaman <aryaman.sriram@gmail.com>
2019-03-24 12:29:00 +0530

    report added

--- 2CC_1.py
+++ 2CC_1.py
@@ -5,13 +5,13 @@ Created on Sun Mar 17 17:42:19 2019
 
 @author: aryaman
 """
-
+#import modules
 import requests
 import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-
+#fetch data of astronauts
 res_1 = requests.get('http://api.open-notify.org/astros.json')
 data_ISS = dict(res_1.json())
 people = []
@@ -20,13 +20,14 @@ for i in data_ISS['people']:
     people.append(i['name'])
 
 
-      
+#fetch data of ISS Position
 res_2 = requests.get('http://api.open-notify.org/iss-now.json')
 data_ISS_pos = dict(res_2.json())
         
 iss_pos_lat = data_ISS_pos['iss_position']['latitude']
 iss_pos_long = data_ISS_pos['iss_position']['longitude']
         
+#Plot
 fig = plt.figure()
 ax = plt.axes(projection=ccrs.Mollweide())
 ax.stock_img()
@@ -35,7 +36,7 @@ ax.plot([iss_pos_lat, iss_pos_long],color='blue', linewidth=2, marker='o',transf
         
 plt.show()
 
-
+#Print names of astronauts
 print("The ISS Heroes are:-")
 for i in people:
         print(i)
